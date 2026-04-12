2fa1ea960a7e3fee7776c9f2e43d27dbe9298b23
Karthik Gundelly <karthikgundelly67@gmail.com>
2026-04-12 12:33:16 +0530

    added about.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/about.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <h2 className='header'>About Us!</h2>
+      <p>we are hear to assist every transactio  you do</p>
+      <p>we make dashboards you are using with daily</p>
+    </div>
+  )
+}
+
+export default About
